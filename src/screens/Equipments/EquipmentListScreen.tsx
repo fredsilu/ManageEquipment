43f144f8c87f485bbcd2066cac3b8ee85a4cf1ea
Frederic Siluvangi <fredsilu@gmail.com>
2025-01-14 19:23:44 +0100

    modification equipmentlist

--- src/screens/Equipments/EquipmentListScreen.tsx
+++ src/screens/Equipments/EquipmentListScreen.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import { View, Text, FlatList, Button, StyleSheet } from 'react-native';
 
-const EquipmentListScreen: React.FC = ({ navigation }) => {
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RouteProp } from '@react-navigation/native';
+
+type RootStackParamList = {
+  EquipmentList: undefined;
+  EquipmentDetails: { id: string };
+  EquipmentForm: undefined;
+};
+
+type EquipmentListScreenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'EquipmentList'
+>;
+
+type Props = {
+  navigation: EquipmentListScreenNavigationProp;
+};
+
+const EquipmentListScreen: React.FC<Props> = ({ navigation }) => {
   const equipments = [
     { id: '1', name: 'Pump A', type: 'Hydraulic' },
     { id: '2', name: 'Generator B', type: 'Electrical' },
+    { id: '3', name: 'Compressor C', type: 'Pneumatic' },
+    { id: '4', name: 'Valve D', type: 'Hydraulic' },
+    { id: '5', name: 'Motor E', type: 'Electrical' },
+    { id: '6', name: 'Conveyor F', type: 'Mechanical' },
+    { id: '7', name: 'Sensor G', type: 'Electronic' },
+    { id: '8', name: 'Boiler H', type: 'Thermal' },
+    { id: '9', name: 'Fan I', type: 'Mechanical' },
+    { id: '10', name: 'Switch J', type: 'Electrical' },
+    { id: '11', name: 'Transformer K', type: 'Electrical' },
+    { id: '12', name: 'Filter L', type: 'Hydraulic' }
   ];
 
   return (
